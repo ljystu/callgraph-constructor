289aa067d38540db9056af33d8db504c54754799
ljystu <ljystu@gmail.com>
2023-04-15 11:58:17 +0200

    test

--- call-graph-analysis/src/main/java/ljystu/project/callgraph/utils/PackageUtil.java
+++ call-graph-analysis/src/main/java/ljystu/project/callgraph/utils/PackageUtil.java
@@ -81,7 +81,8 @@ public class PackageUtil {
         String argLine = Constants.ARG_LINE_LEFT + Constants.JAVAAGENT_HOME + "=";
 //                + Constants.ARG_LINE_RIGHT;
         packageScan.append(argLine);
-        Pattern excludedPattern = Pattern.compile(readExcludedPackages());
+        Pattern excludedPattern = Pattern.compile("^((org\\.junit)|(org\\.junit\\.jupiter)|(org\\.testng)|(org\\.mockito)|(org\\.powermock)|(org\\.easymock)|(org\\.hamcrest)|(org\\.assertj\\.core\\.api)|(io\\.cucumber)|(org\\.spockframework)).*");
+//                readExcludedPackages());
 
         HashSet<String> packagePrefixSet = new HashSet<>();
         for (String definedPackage : definedPackages) {
@@ -146,15 +147,15 @@ public class PackageUtil {
 
                 extractPackagesToMap(inclPackages, jar, coord, packagePrefix);
 
-                ProjectUtil.deleteFile(jar);
+//                ProjectUtil.deleteFile(jar);
 
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-        for (File jarFile : jarFiles) {
-            ProjectUtil.deleteFile(jarFile);
-        }
+//        for (File jarFile : jarFiles) {
+//            ProjectUtil.deleteFile(jarFile);
+//        }
         return inclPackages;
     }
 
